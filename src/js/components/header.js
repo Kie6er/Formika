73e1c275441979b065180fcaf30ebd411a082fce
Kie6er <kirill.zawgorodny@yandex.ru>
2023-10-28 19:53:29 +0300

    main page ready

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,1 +1,17 @@
-
+import $ from "jquery";
+$(document).ready(function () {
+	backgroundHeader();
+	$(window).on('scroll', backgroundHeader);
+	function backgroundHeader() {
+		if ($(window).scrollTop() > 0) {
+			$('.header').addClass('scroll');
+		} else {
+			$('.header').removeClass('scroll');
+		}
+		if (($(window).scrollTop()) >= $('.main-banner').outerHeight()) {
+			$('.header').addClass('black');
+		} else {
+			$('.header').removeClass('black');
+		}
+	}
+})
