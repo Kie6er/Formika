8d5eb6c46267c2649bb2bfe305aed9cd6e3db6e0
Kie6er <kirill.zawgorodny@yandex.ru>
2023-10-30 15:36:59 +0300

    fix animation and add validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import $ from "jquery";
+import IMask from 'imask';
 // import Swiper from 'swiper';
 // import 'swiper/css';
 
@@ -9,7 +10,15 @@ import { ScrollSmoother } from "./js/utils/ScrollSmoother.min";
 import "./index.scss";
 import "./js/pages/main-page";
 import "./js/components/header";
+import "./js/components/form";
 import * as constants from "./js/utils/constants";
+
+const phone = document.querySelectorAll('[type=tel]');
+phone.forEach(function (element) {
+	const mask = IMask(element, {
+		mask: '+0 (000) 000-00-00'
+	});
+})
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 if (ScrollTrigger.isTouch !== 1) {
