c346102836bd5cb78a1f8355d58e3fdf5556749e
Kie6er <kirill.zawgorodny@yandex.ru>
2023-11-04 16:08:20 +0300

    fix animation

--- src/js/pages/main-page.js
+++ src/js/pages/main-page.js
@@ -3,46 +3,47 @@ import $ from "jquery";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+
 $(document).ready(function () {
+	ScrollTrigger.refresh();
+
+	let solutionsTimeline = gsap.timeline({
+		defaults: {
+			scrollTrigger: {
+				trigger: '.main-solutions',
+				start: '30% bottom',
+				end: '-10% top',
+				ease: "power2.inOut",
+				scrub: 2,
+				toggleActions: "play complete complete complete"
+			}
+		}
+	});
+	let mainProjectsSession = gsap.timeline();
+	let mobileTimeline = gsap.timeline();
 	// GSAP анимации
 	if ($('.main-solutions').length > 0 && $('.main-projects').length > 0) {
-
 		if (ScrollTrigger.isTouch !== 1) {
-
-			const tl1 = gsap.timeline({
-				defaults: {
-					scrollTrigger: {
-						trigger: '.main-solutions',
-						start: '30% bottom',
-						end: '-10% top',
-						ease: "power2.inOut",
-						scrub: 2,
-						toggleActions: "play complete complete complete"
-					}
-				}
-			});
-
 			let leftItems = gsap.utils.toArray('.main-projects__content-left .main-projects__content-item');
 			let rightItems = gsap.utils.toArray('.main-projects__content-right .main-projects__content-item');
 
 			let leftItemsImg = gsap.utils.toArray('.main-projects__content-left .main-projects__content-item img');
 			let rightItemsImg = gsap.utils.toArray('.main-projects__content-right .main-projects__content-item img');
-			tl1.fromTo('.main-solutions__inner', { y: '20rem', }, { y: '0rem', duration: 1.5, })
+			solutionsTimeline.fromTo('.main-solutions__inner', { y: '20rem', }, { y: '0rem', duration: 1.5, })
 				.fromTo('.main-solutions__wrapper-title--top', { x: '-20rem', }, { x: '0rem', duration: 1.2, })
 				.fromTo('.main-solutions__wrapper-title--bottom', { x: '10rem', }, { x: '0rem', duration: 1.2, })
 				.fromTo('.main-solutions__main-back--overlay', { x: '50rem', }, { x: '0rem', duration: 1.6, })
 				.fromTo('.main-solutions__wrapper-description', { y: '25rem', }, { y: '0rem', duration: 1.6, })
 				.fromTo('.main-solutions__wrapper-text', { y: '25rem', }, { y: '0rem', duration: 1.6, });
 
-			gsap.to('.main-projects__subtitle', {
+			mainProjectsSession.to('.main-projects__subtitle', {
 				scrollTrigger: {
 					trigger: '.main-projects',
 					pin: '.main-projects__subtitle',
 					start: 'top 40%',
 					end: '85% 50%',
 				}
-			})
-			gsap.to('.main-projects__btn', {
+			}).to('.main-projects__btn', {
 				scrollTrigger: {
 					trigger: '.main-projects',
 					pin: '.main-projects__btn',
@@ -52,15 +53,14 @@ $(document).ready(function () {
 			});
 
 			for (let i = 0; i < leftItems.length - 1; i++) {
-				let leftTm = gsap.timeline();
-				leftTm.fromTo('.main-projects__content-left', { y: "20rem" }, {
+				mainProjectsSession.fromTo('.main-projects__content-left', { y: "20rem" }, {
 					y: "0rem",
 					delay: 2,
 					scrollTrigger: {
 						trigger: '.main-projects',
 						start: "-10% center",
 						end: "-2% center",
-						toggleActions: "play none reverse none",
+						toggleActions: "play complete complete complete",
 						scrub: 3,
 					}
 				}).add(swipe)
@@ -70,21 +70,21 @@ $(document).ready(function () {
 							trigger: leftItems[0],
 							start: "50% center",
 							end: "70% center",
-							toggleActions: "play none reverse none",
+							toggleActions: "play complete complete complete",
 							scrub: 3,
 						}
 					})
 				function swipe() {
 					leftItems.forEach((el, i) => {
 						if (i !== 0) {
-							leftTm.to(leftItems[i], {
+							mainProjectsSession.to(leftItems[i], {
 								startAt: {
 									y: "20rem",
 								},
 								y: "0rem",
 								scrollTrigger: {
 									trigger: leftItems[i],
-									toggleActions: "play none reverse none",
+									toggleActions: "play complete complete complete",
 									start: `${- leftItems[i].clientHeight * 2.4} center`,
 									end: `${- leftItems[i].clientHeight * 1.2} center`,
 									scrub: 2,
@@ -92,7 +92,7 @@ $(document).ready(function () {
 							})
 						}
 						if (i !== 0 && i !== (leftItems.length - 1)) {
-							leftTm.to(leftItems[i], {
+							mainProjectsSession.to(leftItems[i], {
 								startAt: {
 									y: "0rem"
 								},
@@ -101,12 +101,12 @@ $(document).ready(function () {
 									trigger: leftItems[i],
 									start: "40% center",
 									end: "60% center",
-									toggleActions: "play none reverse none",
+									toggleActions: "play complete complete complete",
 									scrub: 3,
 								}
 							})
 						}
-						leftTm.to(leftItemsImg[i], {
+						mainProjectsSession.to(leftItemsImg[i], {
 							startAt: {
 								objectPosition: "50% 80%",
 							},
@@ -115,25 +115,23 @@ $(document).ready(function () {
 								trigger: leftItems[i],
 								start: `top bottom`,
 								end: `bottom top`,
-								toggleActions: "play none reverse none",
+								toggleActions: "play complete complete complete",
 								scrub: 2,
 							}
 						})
-						return (leftTm)
+						return (mainProjectsSession)
 					})
 				}
 			}
-
 			for (let i = 0; i < rightItems.length - 1; i++) {
-				let rightTm = gsap.timeline();
-				rightTm.fromTo('.main-projects__content-right', { y: "20rem" }, {
+				mainProjectsSession.fromTo('.main-projects__content-right', { y: "20rem" }, {
 					y: "0rem",
 					scrollTrigger: {
 						trigger: '.main-projects',
 						start: "-2% center",
 						end: "6% center",
 						scrub: 3,
-						toggleActions: "play none reverse none",
+						toggleActions: "play complete complete complete",
 					}
 				}).add(swipeR)
 					.to(rightItems[0], {
@@ -142,14 +140,14 @@ $(document).ready(function () {
 							trigger: leftItems[0],
 							start: "50% center",
 							end: "70% center",
-							toggleActions: "play none reverse none",
+							toggleActions: "play complete complete complete",
 							scrub: 3,
 						}
 					})
 				function swipeR() {
 					rightItems.forEach((el, i) => {
 						if (i !== 0) {
-							rightTm.to(rightItems[i], {
+							mainProjectsSession.to(rightItems[i], {
 								duration: 1,
 								startAt: {
 									y: "20rem",
@@ -158,14 +156,14 @@ $(document).ready(function () {
 								scrollTrigger: {
 									trigger: rightItems[i],
 									scrub: 2,
-									toggleActions: "play none reverse none",
+									toggleActions: "play complete complete complete",
 									start: `${- leftItems[i].clientHeight * 2.2} center`,
 									end: `${- leftItems[i].clientHeight * 1.6} center`,
 								}
 							})
 						}
 						if (i !== 0 && i !== (rightItems.length - 1)) {
-							rightTm.to(rightItems[i], {
+							mainProjectsSession.to(rightItems[i], {
 								startAt: {
 									y: "0rem"
 								},
@@ -174,12 +172,12 @@ $(document).ready(function () {
 									trigger: leftItems[i],
 									start: "40% center",
 									end: "60% center",
-									toggleActions: "play none reverse none",
+									toggleActions: "play complete complete complete",
 									scrub: 3,
 								}
 							})
 						}
-						rightTm.to(rightItemsImg[i], {
+						mainProjectsSession.to(rightItemsImg[i], {
 							startAt: {
 								objectPosition: "50% 80%",
 							},
@@ -188,17 +186,19 @@ $(document).ready(function () {
 								trigger: leftItems[i],
 								start: `top bottom`,
 								end: `bottom top`,
-								toggleActions: "play none reverse none",
+								toggleActions: "play complete complete complete",
 								scrub: 2,
 							}
 						})
-						return (rightTm)
+						return (mainProjectsSession)
 					})
 				}
 			}
-		} else {
-			const projectMobileImage = gsap.utils.toArray('.main-projects__content-item img');
-			gsap.to('.main-solutions__main-back', {
+		}
+
+		if (ScrollTrigger.isTouch === 1) {
+			let projectMobileImage = gsap.utils.toArray('.main-projects__content-item img');
+			mobileTimeline.to('.main-solutions__main-back', {
 				startAt: {
 					scale: 0.45
 				},
@@ -208,11 +208,13 @@ $(document).ready(function () {
 					start: "top center",
 					end: "center center",
 					scrub: 2,
+					// toggleActions: "play complete complete complete",
+					// markers: true
 				}
 			})
 
 			projectMobileImage.forEach((el, i) => {
-				gsap.to(projectMobileImage[i], {
+				mobileTimeline.to(projectMobileImage[i], {
 					startAt: {
 						objectPosition: "50% 80%",
 					},
@@ -221,13 +223,37 @@ $(document).ready(function () {
 						trigger: '.main-projects',
 						start: `top bottom`,
 						end: `bottom top`,
-						toggleActions: "play none reverse none",
+						// markers: true,
+						// toggleActions: "play complete complete complete",
 						scrub: 2,
 					}
 				})
 			})
 		}
 	}
+
+	window.addEventListener('beforeunload', function () {
+		localStorage.setItem('solutionsAnimProgress', solutionsTimeline.progress());
+		localStorage.setItem('projectsAnimProgress', mainProjectsSession.progress());
+		localStorage.setItem('mobileAnimProgress', mobileTimeline.progress());
+	})
+
+	window.addEventListener('load', function () {
+		const savedProgressSolutions = localStorage.getItem('solutionsAnimProgress');
+		const savedProgressProjects = localStorage.getItem('projectsAnimProgress');
+		const savedProgressMobile = localStorage.getItem('mobileAnimProgress');
+
+		if (savedProgressMobile || savedProgressProjects || savedProgressSolutions) {
+			solutionsTimeline.progress(parseFloat(savedProgressSolutions));
+			mainProjectsSession.progress(parseFloat(savedProgressProjects));
+			mobileTimeline.progress(parseFloat(savedProgressMobile));
+		}
+
+		ScrollTrigger.refresh();
+	});
+	window.addEventListener('resize', function () {
+		ScrollTrigger.refresh();
+	})
 
 	// Воиспрозвидение видео
 	videoPlay();
